5d1f324d018dbb5263b31a30755c788c716aef0d
JadeonMHC <35738063+JadeonMHC@users.noreply.github.com>
2018-02-27 09:19:26 -0700

    Made API a thing

---
+++ VeganPollSite/VeganPollSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VeganPollSite.WebApiApplication" Language="C#" %>
---
+++ Wagnerett/Wagnerett/PollAPI.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Wagnerett {
+    public partial class PollAPI : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Response.Write("{\"action\":67}");
+        }
+    }
+}
