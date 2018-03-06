9473024a7f969c248fbe7a6588b2d8c3de733878
Mark Wagner <markwagner96@gmail.com>
2018-03-06 13:05:52 -0700

    Changed Poll Object

--- Wagnerett/DBLayer/Poll.cs
+++ Wagnerett/DBLayer/Poll.cs
@@ -8,7 +8,12 @@ namespace DBLayer
 {
     public class Poll
     {
+        public string PollID { get; set; }
         public string Question { get; set; }
+        public DateTime DateCreated { get; }
+        public DateTime EndDate { get; set; }
+        public string Tripcode { get; set; }
+
         public List<PollAnswer> Answers { get; set; }
     }
 }
