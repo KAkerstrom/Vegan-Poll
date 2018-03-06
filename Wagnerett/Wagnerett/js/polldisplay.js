a6433fbc36478807a69f765f62157bf6ecbf22d8
JadeonMHC <35738063+JadeonMHC@users.noreply.github.com>
2018-03-06 10:14:11 -0700

    Started website

---
+++ Wagnerett/Wagnerett/js/polldisplay.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $(".PollBox").append(RadioBox('gr', 'yes', 'Yes'));
+    $(".PollBox").append(RadioBox('gr', 'no', 'No'));
+    $(".PollBox").append(RadioBox('gr', 'maybe', 'Maybe So'));
+});
+
+function RadioBox(group, name, label) {
+
+    var tr = $('<div class="RadioBox">');
+
+    tr.attr('id', name);
+
+    tr.append('<div class="Button"></div>')
+    tr.append($('<div class="Label">').append(label));
+
+    return $('<div class="Answer">').append(tr);
+}
