1a250e053049f1f669c3180cf268893b3ffe413f
Kyle <KAkerstrom@users.noreply.github.com>
2018-03-08 08:58:54 -0700

    Worked on CreatePoll

--- Wagnerett/DBLayer/Poll.cs
+++ Wagnerett/DBLayer/Poll.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace DBLayer
 {
+    public enum AnswerTypes
+    {
+        Single, Multiple
+    }
+
     public class Poll
     {
         public string PollID { get; set; }
@@ -13,7 +18,7 @@ namespace DBLayer
         public DateTime DateCreated { get; }
         public DateTime EndDate { get; set; }
         public string Tripcode { get; set; }
-
+        public AnswerTypes AnswerType { get; set; }
         public List<PollAnswer> Answers { get; set; }
     }
 }
