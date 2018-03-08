60a3c82b2a373e3b37a81ffafb1a1900b81b364e
Mark Wagner <markwagner96@gmail.com>
2018-03-08 10:08:17 -0700

    Added form to test my DBLayer changes.
    Also added methods to open, close, test database connection, and populate a dictionary of answer types.

--- Wagnerett/DBLayer/DBInterface.cs
+++ Wagnerett/DBLayer/DBInterface.cs
@@ -9,12 +9,65 @@ using System.Security.Cryptography;
 
 namespace DBLayer
 {
-    class DBInterface
+    public static class DBInterface
     {
         private const string Server = "172.18.28.12";
-        private const string Database = "Test";
+        private const string Database = "Vegan-Poll";
         private const string UserName = "SQLAdmin";
         private const string Password = "P@$$W0rd";
+
+        private static Dictionary<int, string> AnswerTypeDictionary = new Dictionary<int, string>();
+        private static SqlConnection con;
+
+        static DBInterface()
+        {
+            TestDB();
+            string sql = "SELECT * FROM AnswerTypes";
+            OpenDB();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                AnswerTypeDictionary.Add((int)reader["AnswerTypeID"], (string)reader["Type"]);
+            }
+            CloseDB();
+        }
+        /// <summary>
+        /// Opens and closes database to check for connection problems. May be redundant, should be safe to remove.
+        /// </summary>
+        private static void TestDB()
+        {
+            OpenDB();
+            CloseDB();
+        }
+
+
+        /// <summary>
+        /// Attempts to create and open database connection
+        /// </summary>
+        private static void OpenDB()
+        {
+            if (con == null)
+            {
+                con = new SqlConnection($"Server={Server};Database={Database};User Id={UserName};Password={Password};Connection Timeout=5;");
+            }
+
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                con.Open();
+            }
+        }
+
+        /// <summary>
+        /// Checks if connection exists and is open, then attempts to close database connection
+        /// </summary>
+        private static void CloseDB()
+        {
+            if (con != null && con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
 
         /// <summary>
         /// Generates a 32-character hexadecimal GUID string.
@@ -33,14 +86,8 @@ namespace DBLayer
         /// <returns>Returns whether the operation was successful.</returns>
         public static bool CreatePoll(Poll poll)
         {
-            SqlConnection con = new SqlConnection();
 
-            string conString = $"Server={Server};Database={Database};User Id={UserName};Password={Password};Connection Timeout=5;";
-            string sqlString =
-                "INSERT INTO Polls (PollID, PollQuestion, TimeCreated, EndDate, TripCode, AnswerTypeID) VALUES @pollId, @pollQuestion, @timeCreated, @endDate, @tripCode, @answerTypeId)";
-
-            con.ConnectionString = conString;
-
+            string sqlString = "INSERT INTO Polls (PollID, PollQuestion, TimeCreated, EndDate, TripCode, AnswerTypeID) VALUES @pollId, @pollQuestion, @timeCreated, @endDate, @tripCode, @answerTypeId)";
             SqlCommand command = new SqlCommand(sqlString, con);
             command.Parameters.AddWithValue("@pollId", poll.PollID);
             command.Parameters.AddWithValue("@pollQuestion", poll.Question);
@@ -54,9 +101,9 @@ namespace DBLayer
 
             try
             {
-                con.Open();
+                OpenDB();
 
-                con.Close();
+                CloseDB();
             }
             catch (SqlException sqlex)
             {
@@ -68,11 +115,9 @@ namespace DBLayer
 
         public static Poll GetPoll(int PollID)
         {
-            string conString = $"Server={Server};Database={Database};User Id={UserName};Password={Password};Connection Timeout=5;";
             string sql = "SELECT * FROM Polls WHERE PollID = @pID";
             Poll retPoll = new Poll();
-
-            SqlConnection con = new SqlConnection(conString);
+            OpenDB();
             SqlCommand command = new SqlCommand(sql, con);
             command.Parameters.Add("@pID", SqlDbType.Int);
             command.Parameters["@pID"].Value = PollID;
@@ -84,10 +129,10 @@ namespace DBLayer
                     string treatment = dr[0].ToString();
                 }
             }
-
+            CloseDB();
             return retPoll;
         }
-            
+
 
     }
 }
