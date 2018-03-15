91992e1c9253cf139d93ff80ff914d6fddcce9bc
Mark Wagner <markwagner96@gmail.com>
2018-03-15 10:03:32 -0600

    RREEEEEEEEEE

--- Wagnerett/DebugForm/Form1.cs
+++ Wagnerett/DebugForm/Form1.cs
@@ -20,11 +20,9 @@ namespace DebugForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            if (DBInterface.DeletePoll("f3dc779e56f249e3b2acf666755a35a6"))
-            {
-                MessageBox.Show("Success!");
-            }
+            Poll l = DBInterface.GetPoll("f3dc779e56f249e3b2acf666755a35a6");
+            listBox1.Items.Add(l.Question);
+            listBox1.Items.Add(l.Disabled);
         }
     }
 }
