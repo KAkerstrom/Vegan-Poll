b5b5a06789b384fbcccdefd5bb22294faae60c16
Mark Wagner <markwagner96@gmail.com>
2018-03-15 10:08:49 -0600

    Revert "Merge branch 'master' of https://github.com/KAkerstrom/Vegan-Poll"
    This reverts commit c54e87d260413a5545ea19592a1a56d7b30b7186, reversing
    changes made to 0eab64c86fb81b19075f3fe3187b39d6abcc47de.

--- Wagnerett/DBLayer/Poll.cs
+++ Wagnerett/DBLayer/Poll.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +17,7 @@ namespace DBLayer
         public bool Disabled { get; set; }
         public List<PollAnswer> Answers { get; set; }
 
-        public Poll(string ID, string Quest, DateTime Datecreate, DateTime? End, string trip, int Answer)
+        public Poll(string ID, string Quest, DateTime Datecreate, DateTime End, string trip, int Answer)
         {
             PollID = ID;
             Question = Quest;
@@ -27,16 +26,6 @@ namespace DBLayer
             Tripcode = trip;
             AnswerType = Answer;
             Disabled = false;
-        }
-        public Poll(string ID, string Quest, DateTime Datecreate, DateTime? End, string trip, int Answer, bool dis)
-        {
-            PollID = ID;
-            Question = Quest;
-            DateCreated = Datecreate;
-            EndDate = End;
-            Tripcode = trip;
-            AnswerType = Answer;
-            Disabled = dis;
         }
         public Poll()
         {
