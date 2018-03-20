b102fe556775a7565c57bc1c2e6edaa80a9f9692
Jadeon Sheppard <Jadeon.Sheppard@itp.local>
2018-03-20 08:15:40 -0600

    Added percent bars to website
    It also is completely dynamic

--- Wagnerett/Wagnerett/js/polldisplay.js
+++ Wagnerett/Wagnerett/js/polldisplay.js
@@ -1,4 +1,96 @@
-﻿$(document).ready(function () {
+﻿var DummyPoll = {
+    id: "A67ggBgt298dsdfH67",
+    question: "Do you bees?",
+    type: 1,
+    created: "Feb 28, 2018 6:42AM",
+    closed: null,
+    answers: [
+        {
+            id: 23,
+            text: "Yes",
+            votes: 17
+        },
+        {
+            id: 24,
+            text: "No",
+            votes: 2
+        },
+        {
+            id: 25,
+            text: "Maybe So",
+            votes: 10
+        }
+    ]
+};
+
+function GenVoteBox(poll) {
+    var tr = $('<div class="PollBox">');
+
+    tr.append($('<div class="Question">').append(poll.question));
+
+    poll.answers.forEach(function (answer) {
+        tr.append(RadioBox(poll.id, answer.id, answer.text));
+    });
+
+    tr.append('<div class="SubmitButton">Submit!</div>');
+
+    return tr;
+};
+
+function GenResultsBox(poll) {
+    var tr = $('<div class="PollBox">');
+    var tv = SumVotes(poll);
+
+    tr.append($('<div class="Question">').append(poll.question));
+
+    poll.answers.forEach(function (answer) {
+        var ab = $('<div class="Answer">');
+        var rb = $('<div class="RadioBox">');
+        var lb = $('<div class="Label">');
+
+        var per = Math.floor((answer.votes / tv) * 100.0);
+
+        lb.append(answer.text);
+
+        var pb = $('<div class="Percent">');
+        pb.text(per + "%");
+
+        var tb = $('<div class="Tally">');
+        tb.append(answer.votes + " votes");
+
+        if (per < 50)
+            tb.css({
+                'right': '-105px',
+                'text-align': 'left',
+                'color': '#999'
+            });
+
+        pb.append(tb);
+
+        pb.css('width', per + '%');
+
+        lb.append(pb);
+
+        rb.append(lb);
+        ab.append(rb);
+
+        tr.append(ab);
+    });
+
+    return tr;
+}
+
+function SumVotes(poll) {
+    var tr = 0;
+
+    poll.answers.forEach(function (answer) {
+        tr += answer.votes;
+    });
+
+    return tr;
+}
+
+$(document).ready(function () {
     $(".PollBox").append(RadioBox('gr', 'yes', 'Yes'));
     $(".PollBox").append(RadioBox('gr', 'no', 'No'));
     $(".PollBox").append(RadioBox('gr', 'maybe', 'Maybe So'));
@@ -9,6 +101,9 @@
 
         });
     });
+
+    $("#PollList").append(GenVoteBox(DummyPoll));
+    $("#PollList").append(GenResultsBox(DummyPoll));
 });
 
 function API(action, data, c) {
