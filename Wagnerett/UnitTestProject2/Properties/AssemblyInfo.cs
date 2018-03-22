a35e271626950bfff6bd0ae589442864dd8a55a7
soma588 <35738058+soma588@users.noreply.github.com>
2018-03-21 18:13:45 -0600

    Unit Testing
    I added Tests :)

---
+++ Wagnerett/UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85c05300-4bd1-409e-9507-544060a99365")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Wagnerett/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DBLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string hmmmm;
+        static string haha = "440";
+        static DateTime? NANI = DateTime.Now;
+        static DateTime WHAT = DateTime.Now;
+
+        Poll p = new Poll(haha, "Why even try?", WHAT, null, "", 1);
+        Poll pr;
+        [TestMethod]
+        public void CreatePoll()
+        {
+
+          
+          
+            DBInterface.CreatePoll(p, out string sad, out string meh);
+            
+        }
+        [TestMethod]
+        public void GetPoll()
+        {
+            DBInterface.CreatePoll(p, out string sad, out string meh);
+            pr = DBInterface.GetPoll(sad);
+            Assert.IsNotNull(pr);
+            Assert.AreEqual(p.Question, pr.Question);
+            Assert.AreEqual(p.Disabled, pr.Disabled);
+            Assert.AreEqual(p.Tripcode, pr.Tripcode);
+           // Assert.AreEqual(p.EndDate, pr.EndDate);
+            Assert.AreEqual(p.AnswerType, pr.AnswerType);
+           //Assert.AreEqual(p.DateCreated, pr.DateCreated);
+            Assert.AreEqual(p.PollID, pr.PollID);
+            Assert.AreEqual(p.Answers, pr.Answers);
+            
+        }
+    }
+}
